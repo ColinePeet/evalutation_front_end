8946e6362cb25cf544e0bb81212a183c3f54e1c0
coline peetermans <colpeetermans@gmail.com>
2019-02-25 15:12:45 +0100

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+// swiper
+
+var swiperH = new Swiper('.swiper-container-h', {
+  spaceBetween: 50,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination-h',
+    clickable: true,
+  },
+});
+
